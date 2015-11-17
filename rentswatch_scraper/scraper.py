2f691c2241ae85f0efcde0d5e3d8ee4390cd060a
pirhoo <hello@pirhoo.com>
2015-11-17 15:58:55 +0100

    Remove output colors

--- rentswatch_scraper/scraper.py
+++ rentswatch_scraper/scraper.py
@@ -83,10 +83,10 @@ class Scraper(object):
         except KeyboardInterrupt: print '\nThanks, have a nice day!'
 
     def ok(self, string):
-        print u'\033[92m%s\033[0m' % string.encode("utf-8")
+        print u'✔ ' % string.encode("utf-8")
 
     def fail(self, string):
-        print u'\033[91m%s\033[0m' % string.encode("utf-8")
+        print u'✖ ' % string.encode("utf-8")
 
     def find_ad_blocks(self, soup):
         # By default we consider the ad block is a link
