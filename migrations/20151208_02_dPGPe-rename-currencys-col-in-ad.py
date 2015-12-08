2c1f7abbab50d17bacdfa78763a7007a80bdb7a6
pirhoo <hello@pirhoo.com>
2015-12-08 17:36:01 +0100

    Fixed syntax for MySQL

--- migrations/20151208_02_dPGPe-rename-currencys-col-in-ad.py
+++ migrations/20151208_02_dPGPe-rename-currencys-col-in-ad.py
@@ -7,7 +7,7 @@ from yoyo import step
 __depends__ = {'20151208_01_a2Dh8-add-currency-to-ad'}
 
 steps = [
-    step("ALTER TABLE ad RENAME serviceChargeOriginalCurrency TO service_charge_original_currency"),
-    step("ALTER TABLE ad RENAME baseRentOriginalCurrency TO ba_serent_original_currency"),
-    step("ALTER TABLE ad RENAME totalRentOriginalCurrency TO tot_alrent_original_currency"),
+    step("ALTER TABLE ad CHANGE COLUMN serviceChargeOriginalCurrency service_charge_original_currency FLOAT"),
+    step("ALTER TABLE ad CHANGE COLUMN baseRentOriginalCurrency base_rent_original_currency FLOAT"),
+    step("ALTER TABLE ad CHANGE COLUMN totalRentOriginalCurrency total_rent_original_currency FLOAT"),
 ]
